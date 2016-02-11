60fc60039753570a0966eab9cc3726dbd2587768
Randall Mardus <randalljmardus@gmail.com>
2016-02-11 13:56:22 -0500

    first commit

--- MoneyballGuardDoTryCatch/ViewController.swift
+++ MoneyballGuardDoTryCatch/ViewController.swift
@@ -13,6 +13,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        struct Player {
+            var goodFace: Bool
+            var highOBP: Double
+            var oddball: Bool
+        }
+        
+        enum ValidationErrors: ErrorType {
+            case InvalidFace, InvalidOBP, InvalidOddballness
+        }
+        
+        struct PlayerValidator {
+            var goodFace: Bool?
+            var highOBP: Double?
+            var oddball: Bool?
+            
+            func isMoneyballMaterial() throws -> Player {
+                guard let validFace = goodFace where goodFace != true else {
+                    print("Good looks can't hit")
+                    throw ValidationErrors.InvalidFace
+                }
+                guard let validOBP = highOBP where highOBP > 0.400 else {
+                    print("Dude's weak")
+                    throw ValidationErrors.InvalidOBP
+                }
+                guard let validOddball = oddball where oddball != false else {
+                    print("Must at least be scruffy")
+                    throw ValidationErrors.InvalidOddballness
+                }
+                return Player(goodFace: validFace, highOBP: validOBP, oddball: validOddball)
+            }
+        }
+        
+        let aPlayer = PlayerValidator(goodFace: true, highOBP: 0.297, oddball: false)
+        do {
+            try aPlayer.isMoneyballMaterial()
+        } catch ValidationErrors.InvalidFace {
+            //tell GM to find someone with a face for radio
+        } catch ValidationErrors.InvalidOBP {
+            //tell GM to find someone who likes the beanball
+        } catch ValidationErrors.InvalidOddballness {
+            //tell GM to find someone slightly deformed
+        } catch {
+            // catch any unforseen biases such as GM's relatives.
+        }
     }
 
     override func didReceiveMemoryWarning() {
